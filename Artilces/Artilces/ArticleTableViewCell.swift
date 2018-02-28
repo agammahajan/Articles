d48f2b28cba9ef5f3d2121e280db946e2abdf68c
Agam Mahajan <agam.mahajan@practo.com>
2018-02-28 13:03:52 +0530

    make view of the cell

---
+++ Artilces/Artilces/ArticleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.swift
+//  Artilces
+//
+//  Created by Agam Mahajan on 28/02/18.
+//  Copyright © 2018 Agam Mahajan. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
